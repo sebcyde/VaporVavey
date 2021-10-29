b972fc6b1a88816c373bc75d2c66bedc89ec148b
Seb <sebcyde@hotmail.com>
2021-10-29 21:25:43 +0100

    created a seperate component for Homepage Left Section, about to add state to homepage to switch between nav elements

--- src/Components/HomepageMain/HomepageMain.js
+++ src/Components/HomepageMain/HomepageMain.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './HomepageMain.css';
-import Banner1 from './assets/Banner1.jpg';
+import Home from './HomepageComponents/HomeComponent/Home';
 import FakeAd1 from './assets/fakead1.png';
 import FakeAd2 from './assets/fakead2.png';
 import AOS from 'aos';
@@ -10,44 +10,7 @@ function HomepageMain() {
 	AOS.init();
 	return (
 		<div className="HomepageMainContainer" data-aos="fade-up">
-			<div className="HompageMainLeftSection">
-				<h2 className="HomepageMainTitle">Placeholder Text</h2>
-
-				<p>
-					s the href value. If you cannot provide a valid href, but still need
-					the element to resemble a link, use a button and change it with
-					appropriate styles. Learn more:
-					https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
-					jsx-a11y/anchor-is-valid Line 17:6: The href attribute requires a
-					valid value to be accessible. Provide a valid, navigable address as
-					the href value. If you cannot provide a valid href, but still need the
-					element to resemble a link, use a button and change it with
-					appropriate styles. Learn more:
-					https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
-					jsx-a11y/anchor-is-valid Line 20:6: The href attribute requires a
-					valid value to be accessible. Provide a valid, navigable address as
-					the href value. If you cannot provide a valid href, but still need the
-					element to resemble a link, use a button and change it with
-					appropriate styles. Learn more:
-					https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
-					jsx-a11y/anchor-is-valid Line 23:6: The href attribute requires a
-					valid value to be accessible. Provide a valid, navigable address as
-					the href value. If you cannot provide a valid href, but still need the
-					element to resemble a link, use a button and change it with
-					appropriate styles. Learn more:
-					https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
-					jsx-a11y/anchor-is-valid Line 26:6: The href attribute requires a
-					valid value to be accessible. Provide a valid, navigable address as
-					the href value. If you cannot provide a valid href, but still need the
-					element to resemble a link, use a button and change it with
-					appropriate styles. Learn more:
-					https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
-					jsx-a11y/anchor-is-valid
-				</p>
-
-				<img src={Banner1} className="Banners" data-aos="fade-up" />
-				<p>Fake placeholder ad</p>
-			</div>
+			<Home />
 			<div className="HompageMainRightSection">
 				<p>fake placeholder ads lol</p>
 				<img src={FakeAd1} data-aos="fade-up" />
