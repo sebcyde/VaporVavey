c8cf14f3220ad99eb0ebbdcd281574095c86bc00
Seb <sebcyde@hotmail.com>
2021-10-31 21:01:18 +0000

    created NewCollection 95 Component, added in structure, need to add in images

---
+++ src/Components/HomepageMain/HomepageComponents/RecentNews/NewCollection95/NewCollection95.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './NewCollection95.css';
+
+function NewCollection95() {
+	return (
+		<div>
+			<div className="NewCollection95">
+				<ul className="NewItemsRow1">
+					<li className="Row1Item1">
+						<h3></h3>
+						<img src="" />
+					</li>
+					<li className="Row1Item2">
+						<h3></h3>
+						<img src="" />
+					</li>
+					<li className="Row1Item3">
+						<h3></h3>
+						<img src="" />
+					</li>
+				</ul>
+				<ul className="NewItemsRow2">
+					<li className="Row2Item1">
+						<h3></h3>
+						<img src="" />
+					</li>
+					<li className="Row2Item2">
+						<h3></h3>
+						<img src="" />
+					</li>
+					<li className="Row2Item3">
+						<h3></h3>
+						<img src="" />
+					</li>
+				</ul>
+				<ul className="NewItemsRow3">
+					<li className="Row3Item1">
+						<h3></h3>
+						<img src="" />
+					</li>
+					<li className="Row3Item2">
+						<h3></h3>
+						<img src="" />
+					</li>
+					<li className="Row3Item3">
+						<h3></h3>
+						<img src="" />
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default NewCollection95;
